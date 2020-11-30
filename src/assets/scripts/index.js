2e345d9f17dd7bbfa6b894db5e4262615edb1f6e
rogermedico <roger.medico@gmail.com>
2020-11-30 17:18:00 +0100

    initial commit

---
+++ src/assets/scripts/index.js
@@ -0,0 +1,30 @@
+/* Common polyfills to support old browsers */
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+/* Polyfill to support fetch */
+import 'whatwg-fetch';
+
+import { getData } from './getData';
+import { buildHome } from './buildHome';
+import { buildCategory } from './buildCategory';
+import { buildBook } from './buildBook';
+import { iAmInBook, iAmInCategory, iAmInHome } from './url';
+
+(async () => {
+
+  /* get data */
+  const data = await getData();
+  
+  /* build page */
+  if(iAmInHome()){
+    buildHome(data);
+  }
+  else if(iAmInCategory()){
+    buildCategory(data);
+  }
+  else if(iAmInBook()){
+    buildBook(data);
+  }
+
+})();
